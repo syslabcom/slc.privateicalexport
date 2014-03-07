61cadd72e83f5ab8cea9e9999b38d8426e5deefb
Cillian de Róiste <goibhniu@fsfe.org>
2014-03-07 16:52:10 +0100

    Use the adapter and implementer decorators
    implements and adapts are deprecated

--- slc/privateicalexport/behaviors.py
+++ slc/privateicalexport/behaviors.py
@@ -1,16 +1,16 @@
 from plone.app.content.interfaces import INameFromTitle
 from zope.interface import Interface
-from zope.interface import implements
-from zope.component import adapts
+from zope.interface import implementer
+from zope.component import adapter
 import random
 
 
 class INameFromUUID(Interface):
     pass
 
+@adapter(INameFromUUID)
+@implementer(INameFromTitle)
 class NameFromUUID(object):
-    implements(INameFromTitle)
-    adapts(INameFromUUID)
 
     def __init__(self, context):
         self.title = getattr(context, "_plone.uuid")
