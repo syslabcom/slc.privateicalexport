dedb4f9af97e39771da4cb824a9e306a49cf9530
Cillian de Róiste <goibhniu@fsfe.org>
2014-03-10 16:22:39 +0100

    setup: add icalendar as a dependency

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'icalendar',
         'setuptools',
         'plone.api',
         'plone.app.dexterity',
